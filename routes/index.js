1c4402ec7565e07dce0dcd5c21fcc01948099bb4
Two2code <two2code@gmail.com>
2016-12-23 12:46:14 +0000

    added buttom to index

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
 router.get('/', function(req, res, next){
-    res.render('index',
-      {
-          title:'Name of the game',
-          author:'Hongyu Zheng',
-          description:'A simple recorder for T-E'
-      });
+    res.render('index');
 });
 
-module.exports = router;
+module.exports = router; 
